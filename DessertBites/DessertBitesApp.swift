d6e71da7ced285d51fe176fbb5e342e3fe8f9d3b
keshew <artyom.korotckov2017@yandex.ru>
2025-01-13 17:02:50 +0500

    Initial Commit

---
+++ DessertBites/DessertBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertBitesApp.swift
+//  DessertBites
+//
+//  Created by Артём Коротков on 13.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct DessertBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
